dec5076ce78691b0547fd611cc92324fbc5766f0
wilfredoo <cacao_de_verdad@protonmail.com>
2022-03-22 14:45:57 +0100

    Fix app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import { useForm } from "react-hook-form";
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import { Nav } from "./Nav.jsx";
 
-class App extends Component {
-  render(){
-    return (
-      <div>
-        <Nav/>
-        <Outlet/>
-      </div>
-    );
-  }
+
+function App() {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
+  
+  return (
+    <div>
+      <Nav />
+      <Outlet />
+
+      <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+        <div>
+          <input {...register("Username")} placeholder="Username" />
+        </div>
+        <div>
+          <input {...register("Password")} placeholder="Password" />
+        </div>
+        <input type="submit" />
+        <p>{data}</p>
+      </form>
+
+    </div>
+  );
 }
 
 export default App;
