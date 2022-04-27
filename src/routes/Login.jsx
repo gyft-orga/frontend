e1d24c4000aaa2b67a96d19c63cfbffaf7cb32f8
Jonathan Neidel <git@jneidel.com>
2022-04-27 09:12:10 +0200

    Cleanup Login a little

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import { useForm } from "react-hook-form";
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export function Login() {
   let navigate = useNavigate();
@@ -9,35 +10,28 @@ export function Login() {
   const [data, setData] = useState("");
 
   const onSubmit = data => {
-    axios
-     .post(
-        'http://localhost:8002/auth/login',
-        data,
-        {
-          withCredentials: true,
-          headers: { 'Content-Type': 'application/json' },
-        }
-      )
-     .then(response => {
-        navigate("../home", { replace: true });
+    axios.post( 'http://localhost:8002/auth/login', data, {
+      withCredentials: true,
+      headers: { 'Content-Type': 'application/json' },
       } )
-     .catch(error => {console.log("login error? ", error)});
- };
+    .then( res => navigate("../home", { replace: true }));
+  };
+
+  const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+    width: 200px;
+  `;
 
   return (
     <main>
       <h2>Login</h2>
-       {/* <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}> */}
-       <form onSubmit={handleSubmit((data) => onSubmit(data))}>
-        <div>
+        <Form onSubmit={handleSubmit((data) => onSubmit(data))}>
           <input {...register("username")} placeholder="Username" type="text" />
-        </div>
-        <div>
           <input {...register("password")} placeholder="Password" type="password" />
-        </div>
-        <input type="submit" />
-        {/* <p>{data}</p> */}
-      </form>
+          <input type="submit" />
+        </Form>
     </main>
   );
 }
